1120366f06bde04434d93e2ce87f936eb15e2431
shady ahmed <shadiex@hotmail.com>
2020-05-21 09:55:58 +0200

    Updates

---
+++ precache-manifest.237f41291a6bea359ebc2f862dd9663c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94ab660afeb1b75b50c73e13ef9e0b5",
+    "url": "/carrefour-landing-page/index.html"
+  },
+  {
+    "revision": "a3afa23980925adc35b6",
+    "url": "/carrefour-landing-page/static/css/3.7e7c3680.chunk.css"
+  },
+  {
+    "revision": "e6d0c843d9cf83fc2917",
+    "url": "/carrefour-landing-page/static/css/main.451308b7.chunk.css"
+  },
+  {
+    "revision": "f2533b900e4dfe4225dd",
+    "url": "/carrefour-landing-page/static/js/2.8ebe65f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carrefour-landing-page/static/js/2.8ebe65f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3afa23980925adc35b6",
+    "url": "/carrefour-landing-page/static/js/3.347c22cb.chunk.js"
+  },
+  {
+    "revision": "e6d0c843d9cf83fc2917",
+    "url": "/carrefour-landing-page/static/js/main.06a069fe.chunk.js"
+  },
+  {
+    "revision": "77e9fe07abbc08c72ccb",
+    "url": "/carrefour-landing-page/static/js/runtime-main.5ba7e8e9.js"
+  },
+  {
+    "revision": "5334e060cf9fd0efce798220b5e77c12",
+    "url": "/carrefour-landing-page/static/media/Layer 10.5334e060.png"
+  },
+  {
+    "revision": "54c6c585c71db27aa4842bcf384eb2b1",
+    "url": "/carrefour-landing-page/static/media/Layer 11.54c6c585.png"
+  },
+  {
+    "revision": "c432c47e0005ddb09ced79de456b3335",
+    "url": "/carrefour-landing-page/static/media/Layer 12.c432c47e.png"
+  },
+  {
+    "revision": "b2ca1b4097e00b99401888dc685e40ad",
+    "url": "/carrefour-landing-page/static/media/Layer 13.b2ca1b40.png"
+  },
+  {
+    "revision": "c50b657f925a31ead454a1c99966fc55",
+    "url": "/carrefour-landing-page/static/media/Layer 14.c50b657f.png"
+  },
+  {
+    "revision": "aaf48fbb53282cef02454346bd47068b",
+    "url": "/carrefour-landing-page/static/media/Layer 16.aaf48fbb.png"
+  },
+  {
+    "revision": "d7f917f2bd362b3a017e97aa6849b816",
+    "url": "/carrefour-landing-page/static/media/Layer 17.d7f917f2.png"
+  },
+  {
+    "revision": "c2f97c3ad2be8886027d5b2f0ae59051",
+    "url": "/carrefour-landing-page/static/media/Layer 20.c2f97c3a.png"
+  },
+  {
+    "revision": "0c903aea5159bae066fbd702c197ab52",
+    "url": "/carrefour-landing-page/static/media/Layer 21.0c903aea.png"
+  },
+  {
+    "revision": "c6b29f4ef4fcbe973cbecaac3cd8416a",
+    "url": "/carrefour-landing-page/static/media/Layer 22.c6b29f4e.png"
+  },
+  {
+    "revision": "d2942f59ed70e6c199660e70355f4c45",
+    "url": "/carrefour-landing-page/static/media/Layer 27.d2942f59.png"
+  },
+  {
+    "revision": "26e746112d69a6ef87e4b38070f1e0f0",
+    "url": "/carrefour-landing-page/static/media/Layer 28.26e74611.png"
+  },
+  {
+    "revision": "6494db2f521bea78d542b98569b2e435",
+    "url": "/carrefour-landing-page/static/media/Layer 29.6494db2f.png"
+  },
+  {
+    "revision": "8aa5a2363970632afc5702690da8731c",
+    "url": "/carrefour-landing-page/static/media/Layer 30.8aa5a236.png"
+  },
+  {
+    "revision": "b07013cec617f5e124c21b522fd1a842",
+    "url": "/carrefour-landing-page/static/media/Layer 31.b07013ce.png"
+  },
+  {
+    "revision": "8d06711662866fd8190ab59d31120a1b",
+    "url": "/carrefour-landing-page/static/media/Layer 32.8d067116.png"
+  },
+  {
+    "revision": "58f3a29f0c11166fa8d437921afdd6fe",
+    "url": "/carrefour-landing-page/static/media/Layer 33.58f3a29f.png"
+  },
+  {
+    "revision": "00eaae88006c00585f06baf0053dec15",
+    "url": "/carrefour-landing-page/static/media/Layer 34.00eaae88.png"
+  },
+  {
+    "revision": "3cc69c319343791251bd3e3af6f6de84",
+    "url": "/carrefour-landing-page/static/media/Layer 9.3cc69c31.png"
+  }
+]);
